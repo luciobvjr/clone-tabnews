688ee9bdd01ec1311397b2a2b00ea84c0b4308d1
Lucio Bueno Vieira Junior <luciobvjr@gmail.com>
2023-07-16 23:18:01 -0300

    adiciona scripts 'lint:check' e 'lint:fix'

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
-Projeto relacionado ao curso do Filipe Deschamps para implementação do site https://tabnews.com.br
+
+Projeto relacionado ao curso do Filipe Deschamps para implementação do site <https://tabnews.com.br>
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,13 @@
 function Home() {
-    return (
-        <div>
-            <h1>Se você recebeu essa mensagem, fique sabendo que eu gosto muito de você!!! ❤️.</h1>
-            <h2>de: Lucio</h2>
-        </div>
-    )
+  return (
+    <div>
+      <h1>
+        Se você recebeu essa mensagem, fique sabendo que eu gosto muito de
+        você!!! ❤️.
+      </h1>
+      <h2>de: Lucio</h2>
+    </div>
+  );
 }
 
 export default Home;
